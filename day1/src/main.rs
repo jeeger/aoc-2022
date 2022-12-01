fd5520a3e005a9a979e9a9eed3ed25e6db9e4ba6
Jan Seeger <jan.seeger@thenybble.de>
2022-12-01 10:08:25 +0100

    Day 1: Used reduce

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,63 +1,41 @@
 use std::fs::File;
-use std::path::Path;
-use std::io::BufReader;
 use std::io::BufRead;
-use std::collections::BinaryHeap;
+use std::io::BufReader;
 
-fn solution2(filename: &str) -> Result<(), std::io::Error> {
-    let r = BufReader::new(File::open(filename)?);
-    let mut h = BinaryHeap::new();
-    let mut current_sum: u32 = 0;
-    for line in r.lines() {
-        match line {
-            Ok(s) if s.is_empty() => {
-                h.push(current_sum);
-                current_sum = 0;
-            }
-            Ok(s) => {
-                current_sum += s.parse::<u32>().expect("Failed to parse number");
-            }
-            Err(e) => {
-                return Err(e);
-            }
-        }
-    }
-    let mut solution = 0;
-    for _n in 1..4 {
-        solution += h.pop().expect("Empty heap");
-    }
-    print!("Solution 2: {}\n", solution);
-    Ok(())
+fn lines(filename: &str) -> impl Iterator<Item = String> {
+    let r = BufReader::new(File::open(filename).expect("Failed to open file"));
+    r.lines().map(|elem| elem.expect("Couldn't read line"))
 }
 
-fn solution1(filename: &str) -> Result<(), std::io::Error> {
-    let mut current_max = 0;
+fn solution_generic(filename: &str) -> Vec<u32> {
     let mut current_sum = 0;
-    let path = Path::new(filename);
-    let f = File::open(path)?;
-    let buf = BufReader::new(f);
-    for line in buf.lines() {
-        match line {
-            Ok(s) if s.is_empty() => {
-                if current_sum > current_max {
-                    current_max = current_sum;
-                }
-                current_sum = 0;
-            }
-            Ok(s) => {
-                let num: u32 = s.parse().expect("Failed to parse number");
-                current_sum += num;
-            }
-            Err(e) => {
-                return Err(e);
-            }
+    lines(filename).fold(Vec::new(), |mut l: Vec<u32>, s| {
+        if s.is_empty() {
+            l.push(current_sum);
+            current_sum = 0;
+        } else {
+            current_sum += s.parse::<u32>().expect("Failed to parse number");
         }
-    }
-    print!("Solution 1: {}\n", current_max);
-    Ok(())
+        l
+    })
 }
 
-fn main() -> Result<(), std::io::Error> {
-    solution1("input1.txt")?;
-    solution2("input2.txt")
+fn solution2(filename: &str) {
+    let mut calories = solution_generic(filename);
+    calories.sort();
+    calories.reverse();
+    calories.truncate(3);
+    println!("Solution 2: {}", calories.iter().sum::<u32>())
+}
+
+fn solution1(filename: &str) {
+    let mut calories = solution_generic(filename);
+    calories.sort();
+    calories.reverse();
+    print!("Solution 1: {}\n", calories.first().expect("Empty calories list"));
+}
+
+fn main() {
+    solution1("input1.txt");
+    solution2("input2.txt");
 }
