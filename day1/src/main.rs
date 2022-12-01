583d450528a05fd4f50aa6818ce8a6ca87837bd1
Jan Seeger <jan.seeger@thenybble.de>
2022-12-01 14:17:47 +0100

    Day 1: Added tests

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,61 @@
 use utils::lines;
 
-fn solution_generic(filename: &str) -> Vec<u32> {
+#[cfg(test)]
+mod test {
+    use utils::string_lines;
+    use crate::solution1;
+    use crate::solution2;
+
+    #[test]
+    fn test_solution2() {
+        let test_iter = string_lines(
+            r"
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+",
+        );
+        assert_eq!(solution2(test_iter), 45000)
+    }
+
+    #[test]
+    fn test_solution1() {
+        let test_iter = string_lines(
+            r"
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+",
+        );
+        assert_eq!(solution1(test_iter), 24000);
+    }
+}
+
+fn solution_generic(iter: impl Iterator<Item = String>) -> Vec<u32> {
     let mut current_sum = 0;
-    lines(filename).fold(Vec::new(), |mut l: Vec<u32>, s| {
+    let mut result = iter.fold(Vec::new(), |mut l: Vec<u32>, s| {
         if s.is_empty() {
             l.push(current_sum);
             current_sum = 0;
@@ -10,25 +63,28 @@ fn solution_generic(filename: &str) -> Vec<u32> {
             current_sum += s.parse::<u32>().expect("Failed to parse number");
         }
         l
-    })
+    });
+    // Last entry is not necessarily followed by an empty line.
+    result.push(current_sum);
+    result
 }
 
-fn solution2(filename: &str) {
-    let mut calories = solution_generic(filename);
-    calories.sort();
+fn solution2(iter: impl Iterator<Item = String>) -> u32 {
+    let mut calories = solution_generic(iter);
+    calories.sort_unstable();
     calories.reverse();
     calories.truncate(3);
-    println!("Solution 2: {}", calories.iter().sum::<u32>())
+    calories.iter().sum::<u32>()
 }
 
-fn solution1(filename: &str) {
-    let mut calories = solution_generic(filename);
-    calories.sort();
+fn solution1(iter: impl Iterator<Item = String>) -> u32 {
+    let mut calories = solution_generic(iter);
+    calories.sort_unstable();
     calories.reverse();
-    print!("Solution 1: {}\n", calories.first().expect("Empty calories list"));
+    *calories.first().expect("Empty calories list")
 }
 
 fn main() {
-    solution1("input1.txt");
-    solution2("input2.txt");
+    println!("Solution 1: {}", solution1(lines("input1.txt")));
+    println!("Solution 2: {}", solution2(lines("input2.txt")));
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,8 +1,20 @@
+#![feature(type_alias_impl_trait)]
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Cursor;
 
 pub fn lines(filename: &str) -> impl Iterator<Item = String> {
     let r = BufReader::new(File::open(filename).expect("Failed to open file"));
     r.lines().map(|elem| elem.expect("Couldn't read line"))
+}
+
+pub fn split_lines(filename: &str) -> impl Iterator<Item = Vec<String>> {
+    lines(filename).map(|s| s.split_whitespace().map(|str| String::from(str)).collect())
+}
+
+
+pub fn string_lines(s: &str) -> impl Iterator<Item = String> + '_ {
+    let r = BufReader::new(Cursor::new(s)).lines();
+    r.map(|elem| elem.expect("Couldn't read line"))
 }
